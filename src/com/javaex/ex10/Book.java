23841ef3234eff39586d44aa8e73120fe1315104
pjh990909 <pjh990909@naver.com>
2024-01-08 18:06:41 +0900

    수정

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -1,9 +1,72 @@
 package com.javaex.ex10;
 
 public class Book {
-    
-    private int bookNo;
-    private String title;
-    private String auther;
-    private int stateCode;
+
+	private int bookNo;
+	private String title;
+	private String author;
+	private int stateCode;
+
+	public Book() {
+
+	}
+
+	public Book(int bookNo, String title, String auther) {
+		super();
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author = auther;
+	}
+
+	public Book(int bookNo, String title, String author, int stateCode) {
+		this(bookNo, title, author);
+		this.stateCode = 1;
+	}
+
+	public int getBookNo() {
+		return bookNo;
+	}
+
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+		
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuther(String author) {
+		this.author = author;
+	}
+
+	public int getStateCode() {
+		return stateCode;
+	}
+
+	public void setStateCode(int stateCode) {
+		this.stateCode = stateCode;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [bookNo=" + bookNo + ", title=" + title + ", auther=" + author + ", stateCode=" + stateCode + "]";
+	}
+
+	public void rent() {
+
+	}
+
+	public void print() {
+
+	}
+
 }
