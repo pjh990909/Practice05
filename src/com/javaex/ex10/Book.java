1caff9d679ccf04626f5878d335e74b4f488e866
pjh990909 <pjh990909@naver.com>
2024-01-09 18:09:12 +0900

    과제끝

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -5,7 +5,8 @@ public class Book {
 	private int bookNo;
 	private String title;
 	private String author;
-	private int stateCode;
+	private int stateCode = 1;
+	private String rental = "재고있음";
 
 	public Book() {
 
@@ -18,9 +19,10 @@ public class Book {
 		this.author = auther;
 	}
 
-	public Book(int bookNo, String title, String author, int stateCode) {
+	public Book(int bookNo, String title, String author, String rental, int stateCode) {
 		this(bookNo, title, author);
-		this.stateCode = 1;
+		this.stateCode = stateCode;
+		this.rental = rental;
 	}
 
 	public int getBookNo() {
@@ -29,7 +31,7 @@ public class Book {
 
 	public void setBookNo(int bookNo) {
 		this.bookNo = bookNo;
-		
+
 	}
 
 	public String getTitle() {
@@ -53,25 +55,37 @@ public class Book {
 	}
 
 	public void setStateCode(int stateCode) {
+
+		if (stateCode > -1 && stateCode < 2) {
+			if (stateCode == 0) {
+				this.rental = "대여중";
+			} else if(stateCode == 1){
+				this.rental = "재고있음";
+			}
+		}
 		this.stateCode = stateCode;
+	}
+
+	public String getRental() {
+		return rental;
+	}
+
+	public void setRental(String rental) {
+		this.rental = rental;
 	}
 
 	@Override
 	public String toString() {
-		return "Book [bookNo=" + bookNo + ", title=" + title + ", auther=" + author + ", stateCode=" + stateCode + "]";
+		return "Book [bookNo=" + bookNo + ", title=" + title + ", auther=" + author + ", stateCode=" + rental + "]";
 	}
 
 	public void rent() {
-		if(stateCode==0) {
 			System.out.println(title + "이(가) 대여 됐습니다.");
-		}else {
-			
-		}
 	}
 
 	public void print() {
-		System.out.println(bookNo + " 책 제목:" + title + ", 작가:" + author ", 대여 유무:");
-		
+		System.out.println(bookNo + " 책 제목:" + title + ", 작가:" + author + ", 대여 유무:" + rental);
+
 	}
 
 }
